e7422d494689764d9055ca5b8182236d6d1f8b1f
Nikita <npankow93@gmail.com>
2024-01-21 13:36:09 +0300

    add class ShowPhoneBook

--- task6.java
+++ task6.java
@@ -1,2 +1,23 @@
 class PhoneBook {
     private HashMap<String, HashSet<String>> phoneBook = new HashMap<>();
+     public void addContact(String name, String phoneNumber) {
+        if (phoneBook.containsKey(name)) {
+            HashSet<String> phoneNumbers = phoneBook.get(name);
+            phoneNumbers.add(phoneNumber);
+        } else {
+            HashSet<String> phoneNumbers = new HashSet<>();
+            phoneNumbers.add(phoneNumber);
+            phoneBook.put(name, phoneNumbers);
+        }
+    }
+public void showPhoneBook() {
+        List<Map.Entry<String, HashSet<String>>> sortedEntries = new ArrayList<>(phoneBook.entrySet());
+
+        // Сортировка в убывающем порядке по количеству телефонов
+        sortedEntries.sort((e1, e2) -> Integer.compare(e2.getValue().size(), e1.getValue().size()));
+
+        for (Map.Entry<String, HashSet<String>> entry : sortedEntries) {
+            System.out.println(entry.getKey() + " - " + entry.getValue().size() + " phones: " + entry.getValue());
+        }
+    }
+}
